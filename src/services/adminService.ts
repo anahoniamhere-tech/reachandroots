1c6f8a1d850535d28e344717806f949288246108
anahoniamhere-tech <anahoniamhere@gmail.com>
2026-07-01 21:32:18 +0300

    Fix missing updateDoc import in adminService

--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -1,5 +1,5 @@
 import { 
-  db, auth, collection, getDocs, query, where, writeBatch, doc, deleteDoc
+  db, auth, collection, getDocs, query, where, writeBatch, doc, deleteDoc, updateDoc
 } from '../lib/firebase';
 import { Order, TicketTier } from '../types';
 import { TICKET_TIERS, EVENT_DAYS } from '../constants';
